da692e682e26e9090d536be3512339f5f76b2361
wangto <wangto@192.168.0.105>
2018-08-26 09:22:23 +0800

    add documentation

--- src/com/sample/hashing/consistentHashing/ConsistentHashing.java
+++ src/com/sample/hashing/consistentHashing/ConsistentHashing.java
@@ -1,5 +1,12 @@
 package com.sample.hashing.consistentHashing;
 
+/**
+ * References:
+ * </p>
+ * https://blog.csdn.net/losetowin/article/details/53743135
+ * https://blog.csdn.net/tianpeng341204/article/details/78963850
+ *
+ */
 public class ConsistentHashing {
 
 
