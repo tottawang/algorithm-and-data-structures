67239d044fb94e7c617cad6340e62c841e7abe4b
wangto <wangto@192.168.0.103>
2018-08-26 17:07:24 +0800

    add changes

---
+++ src/com/sample/hashing/consistentHashing/Cluster.java
@@ -0,0 +1,11 @@
+package com.sample.hashing.consistentHashing;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Cluster {
+	
+	private SortedMap<String, String> server = new TreeMap<String, String>();
+	
+
+}
