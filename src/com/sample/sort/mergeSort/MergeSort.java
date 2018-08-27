7f1c2121d143edb84af0d5bef8ef10a1e6cf5dbe
wangto <wangto@192.168.0.102>
2018-08-28 02:00:27 +0800

    add reference

--- src/com/sample/sort/mergeSort/MergeSort.java
+++ src/com/sample/sort/mergeSort/MergeSort.java
@@ -1,5 +1,9 @@
 package com.sample.sort.mergeSort;
 
+/**
+ * Reference https://blog.csdn.net/dd864140130/article/details/50859687
+ *
+ */
 public class MergeSort {
 	
 	public static void sort(int[] arr1, int start, int end, int[] temp) {
